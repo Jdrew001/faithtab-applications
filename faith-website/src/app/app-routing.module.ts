3f13eb81dcdf9fa3fa2c27aca95cdcacebc5ca8d
Drew Atkison <dtatkison@gmail.com>
2019-02-20 23:12:13 -0600

    Updated website to have routing, and modules for shared and home

--- faith-website/src/app/app-routing.module.ts
+++ faith-website/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BiblestudyComponent } from './biblestudy/biblestudy.component';
+import { ContactComponent } from './contact/contact.component';
+import { IamglobalComponent } from './iamglobal/iamglobal.component';
+import { MinistriesComponent } from './ministries/ministries.component';
+import { PlanAVisitComponent } from './plan-a-visit/plan-a-visit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'ministries', component: MinistriesComponent },
+  { path: 'planavisit', component: PlanAVisitComponent },
+  { path: 'iamglobal', component: IamglobalComponent },
+  { path: 'biblestudy', component: BiblestudyComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
